663e557fb3cb267a2822416db5b98b94ccd6d161
Javi970 <javierpaivag@gmail.com>
2023-03-23 15:42:31 -0300

    add delete modal myBooking

--- src/Component/MyBookings.tsx
+++ src/Component/MyBookings.tsx
@@ -6,10 +6,12 @@ import bin from "../assets/icons/delete.svg";
 import QRGenerator from "../commons/PdfDocument/QrGenerator";
 import DocumentPDF from "../commons/PdfDocument/DocumentPDF";
 import { PDFDownloadLink } from "@react-pdf/renderer";
+import DeleteBooking from "../commons/alerts/DeleteBooking";
 
 function MyBookings() {
   const [booking, setBooking] = useState<any>([]);
-
+  const [showModal, setShowModal] = useState(0);
+  const [idBooking, setIdBooking] = useState("");
   const user = useSelector((state: any) => state.user);
 
   useEffect(() => {
@@ -17,10 +19,31 @@ function MyBookings() {
   }, [user]);
 
   const getBooking = async () => {
-    const { data } = await axios.get<any, any>(
-      `http://localhost:3001/api/booking/getBookingOfUser/${user.id}`
-    );
-    setBooking(data);
+    try {
+      const { data } = await axios.get<any, any>(
+        `http://localhost:3001/api/booking/getBookingOfUser/${user.id}`
+      );
+      setBooking(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const asyncFunction = (id: any) => {
+    setIdBooking(id);
+    return setShowModal(1);
+  };
+
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:3001/api/booking/deleteBooking/${idBooking}`)
+      .then((res) => console.log(res))
+      .then(() => window.location.reload())
+      .catch((err) => console.log(err));
+  };
+
+  const asyncFunctionCloseModal = () => {
+    return setShowModal(0);
   };
 
   return (
@@ -37,16 +60,17 @@ function MyBookings() {
           <h1 className="w-full font-roboto text-xl font-semibold p-2 mt-3 mb-3 text-start ">
             Reservas
           </h1>
+
           <div className="lg:flex lg:flex-wrap">
             {booking.length === 0
               ? null
-              : booking.map((turno: any) => (
-                  <div className=" p-2 lg:w-full md:w-1/2">
+              : booking.map((turno: any, i: any) => (
+                  <div className=" p-2 lg:w-full md:w-1/2" key={i}>
                     <div className="justify-between w-full flex items-center border-gray-200 border p-6 rounded-lg">
                       <div className=" grid grid-cols-1 lg:gap-32 lg:grid-cols-4">
                         <div className="w-28">
                           <h2 className="text-grey8 font-roboto font-normal text-xs leading-4">
-                            Nombre y Apellido
+                            Nombre y Apellido{i}
                           </h2>
                           <p className="text-sm font-roboto font-semibold leading-4">
                             {turno.fullName}
@@ -96,7 +120,10 @@ function MyBookings() {
                           </PDFDownloadLink>
                         </button>
 
-                        <button className="bg-red-300 hover:bg-red-400 text-violet font-semibold font-roboto rounded px-3 py-1.5 text-center inline-flex items-center">
+                        <button
+                          className="bg-red-300 hover:bg-red-400 text-violet font-semibold font-roboto rounded px-3 py-1.5 text-center inline-flex items-center"
+                          onClick={() => asyncFunction(booking[i]._id)}
+                        >
                           <img src={bin} alt="" />
                         </button>
                       </div>
@@ -106,6 +133,13 @@ function MyBookings() {
           </div>
         </div>
       </section>
+      {showModal === 1 ? (
+        <DeleteBooking
+          asyncFunction={asyncFunction}
+          handleDelete={handleDelete}
+          asyncFunctionCloseModal={asyncFunctionCloseModal}
+        />
+      ) : null}
     </>
   );
 }
