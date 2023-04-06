8d86da1bbdb1a7c92d27b49ef61b6bba49b949d2
miklzn <mikelzonsarabia@gmail.com>
2023-04-06 15:30:08 -0300

    Estilos Inicio e Iniciar sesion

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,7 +1,10 @@
 import logo from "../assets/image/turnGuy.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const user = useSelector((state: any) => state.user);
+
   return (
     <div className="mt-8 flex flex-col  justify-center w-full h-screen bg-no-repeat bg-cover items-center py-auto ">
       <div className="h-full items-center">
@@ -15,14 +18,31 @@ const Home = () => {
         <h1 className="font-roboto text-center text-4xl font-semibold text-gray-700 ">
           Mi turno WebApp
         </h1>
-        <Link to="/bookingPanel">
-          <button
-            type="submit"
-            className="flex items-center justify-center w-full px-10 py-4 text-lg font-roboto font-bold text-center text-white transition duration-500 ease-in-out transform bg-purple-600 rounded-xl hover:bg-purple-500 focus:outline-none focus:ring-violet-500 mt-8 "
+        {!user.id ? (
+          <Link to={"/login"}>
+            <button
+              type="submit"
+              className="flex items-center justify-center w-full px-10 py-4 text-lg font-roboto font-bold text-center text-white transition duration-500 ease-in-out transform bg-purple-600 rounded-xl hover:bg-purple-500 focus:outline-none focus:ring-violet-500 mt-8 "
+            >
+              Reservar
+            </button>
+          </Link>
+        ) : (
+          <Link
+            to={
+              user.usertype === "operator"
+                ? "/operatorBooking"
+                : "/bookingPanel"
+            }
           >
-            Reservar
-          </button>
-        </Link>
+            <button
+              type="submit"
+              className="flex items-center justify-center w-full px-10 py-4 text-lg font-roboto font-bold text-center text-white transition duration-500 ease-in-out transform bg-purple-600 rounded-xl hover:bg-purple-500 focus:outline-none focus:ring-violet-500 mt-8 "
+            >
+              {user.usertype === "operator" ? "Reservas" : "Reservar"}
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
